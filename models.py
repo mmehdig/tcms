4d9cfcbc15a0135b2262396a1f9961892f5c06bb
mmehdig <mmehdi.g@gmail.com>
2015-10-06 11:22:52 +0200

    move the initial project to github

---
+++ models.py
@@ -0,0 +1,67 @@
+class CoffeeMachine:
+    def __init__(self, config=None):
+        self._valid_size = [1, 2, 3]
+        self._valid_density = [1, 2, 3]
+        self.size = 0
+        self.density = 0
+
+        if config is not None:
+            self.set_size(config['size'])
+            self.set_density(config['density'])
+
+        # clear the message
+        self.message = ""
+
+    def set_size(self, size):
+        size = int(size)
+
+        if size in self._valid_size:
+            self.size = size
+            # success
+            return 0
+        else:
+            self.message = "Warning! Invalid size!"
+            # error code for invalid size
+            return 1
+
+    def set_density(self, density):
+        density = int(density)
+
+        if density in self._valid_density:
+            self.density = density
+            # success
+            return 0
+        else:
+            self.message = "Warning! Invalid density!"
+            # error code for invalid density
+            return 1
+
+    def next_size(self):
+        if self.size not in self._valid_size:
+            self.set_size(self._valid_size[0])
+        else:
+            index = self._valid_size.index(self.size)
+            if index + 1 == len(self._valid_size):
+                self.set_size(self._valid_size[0])
+            else:
+                self.set_size(self._valid_size[index + 1])
+
+    def next_density(self):
+        if self.density not in self._valid_density:
+            self.set_density(self._valid_density[0])
+        else:
+            index = self._valid_density.index(self.density)
+            if index + 1 == len(self._valid_density):
+                self.set_density(self._valid_density[0])
+            else:
+                self.set_density(self._valid_density[index + 1])
+
+        print self.density
+
+    def get_attr(self):
+        return {"size": self.size, "density": self.density}
+
+    def reset(self):
+        self.size = 0
+        self.density = 0
+        self.message = ""
---
+++ run.py
@@ -0,0 +1,84 @@
+"""
+Missing features?
+1. multi user!
+
+"""
+import json
+from flask import Flask, render_template
+from models import CoffeeMachine
+
+app = Flask(__name__)
+
+data_file = open('coffee_machine.json')
+cm = CoffeeMachine(json.load(data_file))
+data_file.close()
+print(cm.__dict__)
+
+
+@app.route("/")
+def index():
+    return render_template('layout.html')
+
+
+@app.route("/cm/interface")
+def interface():
+    return render_template('interface.html', attributes=cm.get_attr())
+
+
+@app.route("/cm")
+def coffee_machine():
+    print json.dumps(cm.__dict__)
+    return json.dumps(cm.__dict__)
+
+
+@app.route("/cm/<size>/<density>")
+def update(size, density):
+    cm.set_size(size)
+    cm.set_density(density)
+    data_file = open('coffee_machine.json', 'w')
+    data_file.write(json.dumps(cm.__dict__))
+    return coffee_machine()
+
+
+@app.route("/cm/click/size")
+def click_size():
+    # send message to DM: user asked for next size!
+    return coffee_machine()
+
+@app.route("/cm/size")
+def next_size():
+    cm.next_size()
+    data_file = open('coffee_machine.json', 'w')
+    data_file.write(json.dumps(cm.__dict__))
+    return coffee_machine()
+
+
+@app.route("/cm/click/density")
+def click_density():
+    # send message to DM: user asked for next density!
+    return coffee_machine()
+
+
+@app.route("/cm/density")
+def next_density():
+    cm.next_density()
+    data_file = open('coffee_machine.json', 'w')
+    data_file.write(json.dumps(cm.__dict__))
+    return coffee_machine()
+
+
+@app.route("/cm/reset")
+def reset():
+    cm.reset()
+    return coffee_machine()
+
+
+@app.route("/cm/serve")
+def serve():
+    # serve the coffee?
+    cm.reset()
+    return coffee_machine()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
